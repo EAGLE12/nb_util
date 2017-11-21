95bb38552a485d0d46c5d4ec22629abeaaea0d09
EAGLE <ferrari.race.beautiful@gmail.com>
2017-11-21 23:31:32 +0900

    tmp

--- lib/nb_util.rb
+++ lib/nb_util.rb
@@ -7,12 +7,11 @@ require 'cli'
 
 module NbUtil
   module_function
+
   directry = "#{Dir.home}"
   def get_name(str)
-    array = str.delete(' ').split(/[\/]/)
+    str.delete(' ').split(/[\/]/)
   end
   name = get_name(directry)
   puts "nb_util says hello, #{name[2]} !!"
 end
-
-
